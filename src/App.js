6eff414ff3c3b75af8e629a4d939b8261771bec7
Y <tominhy77@gmail.com>
2022-11-23 16:47:05 +0700

    xay dung footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Router, Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import Loading from './components/LoadingComponent/Loading';
+import Footer from './components/User/FooterComponent/Footer';
 
 export const history = createBrowserHistory();
 
@@ -10,11 +11,11 @@ export const history = createBrowserHistory();
 function App() {
   return (
 
-    <Router histpry={history}>
+    <Router history={history}>
       <Loading />
       <Switch>
         {/* User Route */}
-
+          <Footer/>
         {/* Admin Route */}
 
         {/* Default Route */}
