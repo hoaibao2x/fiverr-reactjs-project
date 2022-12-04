cb7dc4bf40e0f06c89c8400f56d4524852b0dfb9
hoai_bao <hoaibao572@gmail.com>
2022-12-04 17:14:35 +0700

    hoan thien admin loai cong viec

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -3,14 +3,15 @@ import thunk from 'redux-thunk';
 import { LoadingReducer } from '../redux/LoadingReducer'
 import { JobReducer } from './Admin/reducer/JobReducer';
 import { JobTypeDetailReducer } from './Admin/reducer/JobTypeDetailReducer';
+import { JobTypeReducer } from './Admin/reducer/JobTypeReducer';
 import { ListJobByNameReducer } from './User/reducer/ListJobByNameReducer';
-
 
 const rootReducer = combineReducers({
     LoadingReducer,
     JobReducer,
     JobTypeDetailReducer,
-    ListJobByNameReducer
+    ListJobByNameReducer,
+    JobTypeReducer
 });
 
 export const store = createStore(rootReducer, applyMiddleware(thunk))
